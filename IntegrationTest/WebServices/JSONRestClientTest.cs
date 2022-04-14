dbf844e9ff68ab0abc1c89358559ca00a75e94e1
Jorge Luis Sarmiento Herrera <yorchsarmiento@gmail.com>
2022-04-14 17:33:42 -0500

    Testing CRUD actions

--- IntegrationTest/WebServices/JSONRestClientTest.cs
+++ IntegrationTest/WebServices/JSONRestClientTest.cs
@@ -55,6 +55,7 @@ namespace IntegrationTest
                 {
                     StatusCode = HttpStatusCode.OK,
                     Content = new StringContent(json),
+                    Headers = { { "X-Test-Header", "Test-Value" } }
                 });
 
             var restClient = new JSONRestClient<Tarea>(new HttpClient(mockHandler.Object), _mockLogger);
@@ -97,7 +98,8 @@ namespace IntegrationTest
                 .ReturnsAsync(new HttpResponseMessage
                 {
                     StatusCode = HttpStatusCode.BadRequest,
-                    Content = new StringContent(json)
+                    Content = new StringContent(json),
+                    Headers = { { "X-Test-Header", "Test-Value" } }
                 });
 
             var restClient = new JSONRestClient<Tarea>(new HttpClient(mockHandler.Object), _mockLogger);
@@ -126,7 +128,7 @@ namespace IntegrationTest
                 IsComplete = true
             };
 
-            Peticion<Tarea> peticion = new Peticion<Tarea>(RequestUri)
+            Peticion<Tarea> peticion = new Peticion<Tarea>(RequestUri + "/" + expected.Id)
             {
                 Body = expected,
             };
@@ -144,6 +146,7 @@ namespace IntegrationTest
                 {
                     StatusCode = HttpStatusCode.OK,
                     Content = new StringContent(json),
+                    Headers = { { "X-Test-Header", "Test-Value" } }
                 });
 
             var restClient = new JSONRestClient<Tarea>(new HttpClient(mockHandler.Object), _mockLogger);
@@ -165,14 +168,9 @@ namespace IntegrationTest
         [Fact]
         public async Task ShouldFailOnUpdatingTareaWithRestClient()
         {
-            Peticion<Tarea> peticion = new Peticion<Tarea>(RequestUri)
+            Peticion<Tarea> peticion = new Peticion<Tarea>(RequestUri + "/1")
             {
-                Body = new Tarea
-                {
-                    Id = -1,
-                    Name = "Test invalid update RestClient",
-                    IsComplete = false
-                }
+                Body = new Tarea()
             };
 
             var json = JsonSerializer.Serialize<Tarea>(peticion.Body);
@@ -186,7 +184,8 @@ namespace IntegrationTest
                 .ReturnsAsync(new HttpResponseMessage
                 {
                     StatusCode = HttpStatusCode.BadRequest,
-                    Content = new StringContent(json)
+                    Content = new StringContent(json),
+                    Headers = { { "X-Test-Header", "Test-Value" } }
                 });
 
             var restClient = new JSONRestClient<Tarea>(new HttpClient(mockHandler.Object), _mockLogger);
@@ -205,5 +204,162 @@ namespace IntegrationTest
 
         }
 
+        [Fact]
+        public async Task ShouldDeleteTareaWithRestClient()
+        {
+            Tarea expected = new Tarea();
+
+            Peticion<Tarea> peticion = new Peticion<Tarea>(RequestUri + "/" + expected.Id)
+            {
+                Body = expected,
+            };
+
+            var json = JsonSerializer.Serialize<Tarea>(peticion.Body);
+
+            var mockHandler = new Mock<HttpMessageHandler>();
+            mockHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(json),
+                    Headers = { { "X-Test-Header", "Test-Value" } }
+                });
+
+            var restClient = new JSONRestClient<Tarea>(new HttpClient(mockHandler.Object), _mockLogger);
+            Respuesta<Tarea> respuesta = await restClient.DeleteAsync<Tarea>(peticion);
+                        
+            mockHandler.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Delete),
+                ItExpr.IsAny<CancellationToken>());
+            Assert.NotNull(respuesta);
+            Assert.Equal(200, respuesta.HttpStatus);
+            Assert.NotNull(respuesta.Body);
+        }
+
+        [Fact]
+        public async Task ShouldFailOnDeletingTareaWithRestClient()
+        {
+            Peticion<Tarea> peticion = new Peticion<Tarea>(RequestUri + "/1")
+            {
+                Body = new Tarea()
+            };
+
+            var json = JsonSerializer.Serialize<Tarea>(peticion.Body);
+            var mockHandler = new Mock<HttpMessageHandler>();
+            mockHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Content = new StringContent(json),
+                    Headers = { { "X-Test-Header", "Test-Value" } }
+                });
+
+            var restClient = new JSONRestClient<Tarea>(new HttpClient(mockHandler.Object), _mockLogger);
+
+            Respuesta<Tarea> respuesta = await restClient.DeleteAsync<Tarea>(peticion);
+                        
+            mockHandler.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Delete),
+                ItExpr.IsAny<CancellationToken>());            
+            Assert.NotNull(respuesta);
+            Assert.Equal(400, respuesta.HttpStatus);
+            Assert.Null(respuesta.Body);
+            Assert.NotNull(respuesta.Mensaje);
+
+        }
+
+        [Fact]
+        public async Task ShouldGetSinlgeTareaWithRestClient()
+        {
+            Tarea expected = new Tarea
+            {
+                Id = 1,
+                Name = "Test valid RestClient",
+                IsComplete = false
+            };
+
+            Peticion<Tarea> peticion = new Peticion<Tarea>(RequestUri + "/1");
+
+            var json = JsonSerializer.Serialize<Tarea>(expected);
+
+            var mockHandler = new Mock<HttpMessageHandler>();
+            mockHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(json),
+                    Headers = { { "X-Test-Header", "Test-Value" } }
+                });
+
+            var restClient = new JSONRestClient<Tarea>(new HttpClient(mockHandler.Object), _mockLogger);
+            Respuesta<Tarea> respuesta = await restClient.GetAsync<Tarea>(peticion);
+                        
+            mockHandler.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
+                ItExpr.IsAny<CancellationToken>());
+            Assert.NotNull(respuesta);
+            Assert.Equal(200, respuesta.HttpStatus);
+            Assert.NotNull(respuesta.Body);
+            Assert.InRange(respuesta.Body.Id, 1L, 99L);
+            Assert.Equal(expected.Name, respuesta.Body.Name);
+            Assert.Equal(expected.IsComplete, respuesta.Body.IsComplete);
+        }
+
+        [Fact]
+        public async Task ShouldFailOnGettingSingleTareaWithRestClient()
+        {
+            Peticion<Tarea> peticion = new Peticion<Tarea>(RequestUri + "/1");
+
+            var json = JsonSerializer.Serialize<Tarea>(peticion.Body);
+            var mockHandler = new Mock<HttpMessageHandler>();
+            mockHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.BadRequest,
+                    Content = new StringContent(json),
+                    Headers = { { "X-Test-Header", "Test-Value" } }
+                });
+
+            var restClient = new JSONRestClient<Tarea>(new HttpClient(mockHandler.Object), _mockLogger);
+
+            Respuesta<Tarea> respuesta = await restClient.GetAsync<Tarea>(peticion);
+                        
+            mockHandler.Protected().Verify(
+                "SendAsync",
+                Times.Exactly(1),
+                ItExpr.Is<HttpRequestMessage>(req => req.Method == HttpMethod.Get),
+                ItExpr.IsAny<CancellationToken>());            
+            Assert.NotNull(respuesta);
+            Assert.Equal(400, respuesta.HttpStatus);
+            Assert.Null(respuesta.Body);
+            Assert.NotNull(respuesta.Mensaje);
+
+        }
     }
 }
